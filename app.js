54a3549004cc4f7349a6292d950fe75b42c89b03
pasqualipedro <pasqualipedro@gmail.com>
2021-09-03 01:34:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require(`express`);
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get(`/home`, (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + '/views/home.html');
+});
+
+app.get(`/about`, (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get(`/photos`, (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + '/views/photoGallery.html');
+});
+
+app.get(`/works`, (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + '/views/works.html');
+});

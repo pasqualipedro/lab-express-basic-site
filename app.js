9a92612e3dd318d107bf891f646d4cc67d557589
pasqualipedro <pasqualipedro@gmail.com>
2021-09-04 13:31:46 +0100

    done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require(`express`);
 
 const app = express();
 
+
 app.use(express.static('public'));
 
 app.get(`/home`, (request, response, next) => {
@@ -23,3 +24,5 @@ app.get(`/works`, (request, response, next) => {
     console.log(request);
     response.sendFile(__dirname + '/views/works.html');
 });
+
+app.listen(3000, () => console.log(`App listening on port 3000!! TESTE`));
